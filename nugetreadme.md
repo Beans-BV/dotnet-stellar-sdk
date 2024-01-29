7f32e271657524b0b8d305d428108a67d35861c6
cuongph87 <152460338+cuongph87@users.noreply.github.com>
2024-01-29 11:16:56 +0700

    GitHub action for NuGet release (#5)

---
+++ nugetreadme.md
@@ -0,0 +1,1 @@
+The .NET Stellar SDK facilitates client integration with the Stellar Horizon API server and submission of Stellar transactions. It has two main uses: querying Horizon and building, signing, and submitting transactions to the Stellar network.
--- stellar-dotnet-sdk/SorobanTransactionData.cs
+++ stellar-dotnet-sdk/SorobanTransactionData.cs
@@ -18,8 +18,14 @@ public class SorobanTransactionData
         };
     }
 
-    public static SorobanTransactionData FromXdr(xdr.SorobanTransactionData xdr)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="xdr">The <see cref="xdr.SorobanTransactionData"/> can still be null when its containing <see cref="xdr.Transaction.TransactionExt"/>'s Discriminant is 0.</param>
+    /// <returns></returns>
+    public static SorobanTransactionData? FromXdr(xdr.SorobanTransactionData? xdr)
     {
+        if (xdr == null) return null;
         return new SorobanTransactionData
         {
             ExtensionPoint = ExtensionPoint.FromXdr(xdr.Ext),
