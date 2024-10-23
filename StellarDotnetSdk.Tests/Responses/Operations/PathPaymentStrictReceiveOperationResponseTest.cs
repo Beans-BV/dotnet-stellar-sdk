7c1ff762c8d55c664264aee5f65f6962546e3b64
cuongph87 <152460338+cuongph87@users.noreply.github.com>
2024-10-23 13:43:20 +0700

    Bug fixes (#31)

--- StellarDotnetSdk.Tests/Responses/Operations/PathPaymentStrictReceiveOperationResponseTest.cs
+++ StellarDotnetSdk.Tests/Responses/Operations/PathPaymentStrictReceiveOperationResponseTest.cs
@@ -42,9 +42,7 @@ public class PathPaymentStrictReceiveOperationResponseTest
         {
             From = "GCXKG6RN4ONIEPCMNFB732A436Z5PNDSRLGWK7GBLCMQLIFO4S7EYWVU",
             To = "GA5WBPYA5Y4WAEHXWR2UKO2UO4BUGHUQ74EUPKON2QHV4WRHOIRNKKH2",
-            AssetCode = "EUR",
-            AssetIssuer = "GCQPYGH4K57XBDENKKX55KDTWOTK5WDWRQOH2LHEDX3EKVIQRLMESGBG",
-            AssetType = "credit_alphanum4",
+            AssetType = "native",
             Amount = "10.0",
             SourceAssetCode = "USD",
             SourceAssetIssuer = "GC23QF2HUE52AMXUFUH3AYJAXXGXXV2VHXYYR6EYXETPKDXZSAW67XO4",
@@ -59,9 +57,7 @@ public class PathPaymentStrictReceiveOperationResponseTest
         Assert.AreEqual(operation.Amount, operationTest.Amount);
         Assert.AreEqual(operation.SourceMax, operationTest.SourceMax);
         Assert.AreEqual(operation.SourceAmount, operationTest.SourceAmount);
-        Assert.AreEqual(operation.DestinationAsset,
-            Asset.CreateNonNativeAsset(operationTest.AssetCode, operation.AssetIssuer));
-        Assert.AreEqual(operation.SourceAsset,
-            Asset.CreateNonNativeAsset(operationTest.SourceAssetCode, operation.SourceAssetIssuer));
+        Assert.AreEqual(0, operation.DestinationAsset.CompareTo(operationTest.DestinationAsset));
+        Assert.AreEqual(0, operation.SourceAsset.CompareTo(operationTest.SourceAsset));
     }
 }
