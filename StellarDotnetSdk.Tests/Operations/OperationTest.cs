a4d8b805e74b6300727593927c59401602c36f1e
cuongph87 <152460338+cuongph87@users.noreply.github.com>
2025-03-26 15:30:46 +0700

    Revert "Fix: CreateClaimableBalanceSuccess.BalanceId returning a different va…"
    This reverts commit f89185cb2fe45878efc9a0a30fa1a1126ab03b71.

--- StellarDotnetSdk.Tests/Operations/OperationTest.cs
+++ StellarDotnetSdk.Tests/Operations/OperationTest.cs
@@ -733,13 +733,13 @@ public class OperationTest
     {
         var operation =
             RevokeLedgerEntrySponsorshipOperation.ForClaimableBalance(
-                "00000000929b20b72e5890ab51c24f1cc46fa01c4f318d8d33367d24dd614cfdf5491072");
+                "d1d73327fc560cc09f54a11c7a64180611e1f480f3bf60117e41d19d9593b780");
 
         var xdrOperation = operation.ToXdr();
         var decodedOperation = (RevokeLedgerEntrySponsorshipOperation)Operation.FromXdr(xdrOperation);
 
         Assert.IsNull(decodedOperation.SourceAccount);
-        Assert.AreEqual(((LedgerKeyClaimableBalance)operation.LedgerKey).BalanceId.ToLower(),
+        Assert.AreEqual(((LedgerKeyClaimableBalance)operation.LedgerKey).BalanceId,
             ((LedgerKeyClaimableBalance)decodedOperation.LedgerKey).BalanceId.ToLower());
     }
 
