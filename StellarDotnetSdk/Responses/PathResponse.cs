580d3533744e6e5d5256eb26b9886a18413d9c91
Cuong Pham <cuong.pham@beansapp.com>
2025-12-02 13:04:45 +0700

    fix `PathResponse` class

--- StellarDotnetSdk/Responses/PathResponse.cs
+++ StellarDotnetSdk/Responses/PathResponse.cs
@@ -3,47 +3,78 @@ using System.Text.Json.Serialization;
 using StellarDotnetSdk.Assets;
 
 namespace StellarDotnetSdk.Responses;
-#nullable disable
 
-public class PathResponse : Response
+/// <summary>
+///     Represents a payment path found by the path finding algorithm.
+///     A path is a sequence of assets that can be used to convert from a source asset
+///     to a destination asset through the Stellar decentralized exchange.
+/// </summary>
+public sealed class PathResponse : Response
 {
+    /// <summary>
+    ///     The amount of the destination asset that will be received.
+    ///     Represented as a string to preserve precision.
+    /// </summary>
     [JsonPropertyName("destination_amount")]
-    public string DestinationAmount { get; init; }
+    public required string DestinationAmount { get; init; }
 
+    /// <summary>
+    ///     The type of the destination asset: "native", "credit_alphanum4", or "credit_alphanum12".
+    /// </summary>
     [JsonPropertyName("destination_asset_type")]
-    public string DestinationAssetType { get; init; }
+    public required string DestinationAssetType { get; init; }
 
+    /// <summary>
+    ///     The code of the destination asset. Null for native XLM.
+    /// </summary>
     [JsonPropertyName("destination_asset_code")]
-    public string DestinationAssetCode { get; init; }
+    public string? DestinationAssetCode { get; init; }
 
+    /// <summary>
+    ///     The issuer of the destination asset. Null for native XLM.
+    /// </summary>
     [JsonPropertyName("destination_asset_issuer")]
-    public string DestinationAssetIssuer { get; init; }
+    public string? DestinationAssetIssuer { get; init; }
 
+    /// <summary>
+    ///     The amount of the source asset required for this path.
+    ///     Represented as a string to preserve precision.
+    /// </summary>
     [JsonPropertyName("source_amount")]
-    public string SourceAmount { get; init; }
+    public required string SourceAmount { get; init; }
 
+    /// <summary>
+    ///     The type of the source asset: "native", "credit_alphanum4", or "credit_alphanum12".
+    /// </summary>
     [JsonPropertyName("source_asset_type")]
-    public string SourceAssetType { get; init; }
+    public required string SourceAssetType { get; init; }
 
+    /// <summary>
+    ///     The code of the source asset. Null for native XLM.
+    /// </summary>
     [JsonPropertyName("source_asset_code")]
-    public string SourceAssetCode { get; init; }
+    public string? SourceAssetCode { get; init; }
 
+    /// <summary>
+    ///     The issuer of the source asset. Null for native XLM.
+    /// </summary>
     [JsonPropertyName("source_asset_issuer")]
-    public string SourceAssetIssuer { get; init; }
+    public string? SourceAssetIssuer { get; init; }
 
+    /// <summary>
+    ///     The list of intermediate assets in this path (excluding source and destination).
+    ///     Can be empty if this is a direct exchange.
+    /// </summary>
     [JsonPropertyName("path")]
-    public List<Asset> Path { get; init; }
+    public required List<Asset> Path { get; init; } = [];
 
-    [JsonPropertyName("_links")]
-    public PathResponseLinks Links { get; init; }
-
+    /// <summary>
+    ///     The destination asset object.
+    /// </summary>
     public Asset DestinationAsset => Asset.Create(DestinationAssetType, DestinationAssetCode, DestinationAssetIssuer);
 
+    /// <summary>
+    ///     The source asset object.
+    /// </summary>
     public Asset SourceAsset => Asset.Create(SourceAssetType, SourceAssetCode, SourceAssetIssuer);
-
-    public class PathResponseLinks
-    {
-        [JsonPropertyName("self")]
-        public Link<PathResponse> Self { get; init; }
-    }
 }
