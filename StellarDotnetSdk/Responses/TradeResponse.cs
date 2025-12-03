10aed0e612c4035d1adcfd39cb770a00e024c369
Cuong Pham <cuong.pham@beansapp.com>
2025-12-03 11:08:38 +0700

    f

--- StellarDotnetSdk/Responses/TradeResponse.cs
+++ StellarDotnetSdk/Responses/TradeResponse.cs
@@ -163,7 +163,7 @@ public sealed class TradeResponse : Response, IPagingToken
         ///     Link to the base account information.
         /// </summary>
         /// <remarks>
-        ///     This can either be Link&lt;LiquidityPoolResponse&gt; or Link&lt;LiquidityPoolResponse&gt; depending on the trade
+        ///     This can either be Link&lt;LiquidityPoolResponse&gt; or Link&lt;AccountResponse&gt; depending on the trade
         ///     type.
         /// </remarks>
         [JsonPropertyName("base")]
@@ -173,7 +173,7 @@ public sealed class TradeResponse : Response, IPagingToken
         ///     Link to the counter account information.
         /// </summary>
         /// <remarks>
-        ///     This can either be Link&lt;LiquidityPoolResponse&gt; or Link&lt;LiquidityPoolResponse&gt; depending on the trade
+        ///     This can either be Link&lt;LiquidityPoolResponse&gt; or Link&lt;AccountResponse&gt; depending on the trade
         ///     type.
         /// </remarks>
         [JsonPropertyName("counter")]
