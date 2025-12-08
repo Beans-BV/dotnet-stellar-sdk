ab92c85725b5ac302d17f1e2f093e02025eda968
cuongph87 <152460338+cuongph87@users.noreply.github.com>
2025-12-08 08:37:52 +0700

    fix: `PathResponse` class (#109)

--- StellarDotnetSdk.Tests/Responses/PathsPageDeserializerTest.cs
+++ StellarDotnetSdk.Tests/Responses/PathsPageDeserializerTest.cs
@@ -34,38 +34,41 @@ public class PathsPageDeserializerTest
 
     public static void AssertTestData(Page<PathResponse> pathsPage)
     {
-        Assert.IsNull(pathsPage.NextPage());
-        Assert.IsNull(pathsPage.PreviousPage());
+        Assert.IsNotNull(pathsPage);
+        Assert.IsNull(pathsPage.Links);
 
-        Assert.AreEqual(pathsPage.Records[0].DestinationAmount, "20.0000000");
-        Assert.AreEqual(pathsPage.Records[0].DestinationAsset,
-            Asset.CreateNonNativeAsset("EUR", "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN"));
-        Assert.AreEqual(pathsPage.Records[0].Path.Count, 0);
-        Assert.AreEqual(pathsPage.Records[0].SourceAmount, "30.0000000");
-        Assert.AreEqual(pathsPage.Records[0].SourceAsset,
-            Asset.CreateNonNativeAsset("USD", "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN"));
+        var record1 = pathsPage.Records[0];
+        Assert.AreEqual("20.0000000", record1.DestinationAmount);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("EUR", "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN"),
+            record1.DestinationAsset);
+        Assert.AreEqual(0, record1.Path.Count);
+        Assert.AreEqual("30.0000000", record1.SourceAmount);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("USD", "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN"),
+            record1.SourceAsset);
 
-        Assert.AreEqual(pathsPage.Records[1].DestinationAmount, "50.0000000");
-        Assert.AreEqual(pathsPage.Records[1].DestinationAsset,
-            Asset.CreateNonNativeAsset("EUR", "GBFMFKDUFYYITWRQXL4775CVUV3A3WGGXNJUAP4KTXNEQ2HG7JRBITGH"));
-        Assert.AreEqual(pathsPage.Records[1].Path.Count, 1);
-        Assert.AreEqual(pathsPage.Records[1].Path[0],
-            Asset.CreateNonNativeAsset("GBP", "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN"));
-        Assert.AreEqual(pathsPage.Records[1].SourceAmount, "60.0000000");
-        Assert.AreEqual(pathsPage.Records[1].SourceAsset,
-            Asset.CreateNonNativeAsset("USD", "GBRAOXQDNQZRDIOK64HZI4YRDTBFWNUYH3OIHQLY4VEK5AIGMQHCLGXI"));
+        var record2 = pathsPage.Records[1];
+        Assert.AreEqual("50.0000000", record2.DestinationAmount);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("EUR", "GBFMFKDUFYYITWRQXL4775CVUV3A3WGGXNJUAP4KTXNEQ2HG7JRBITGH"),
+            record2.DestinationAsset);
+        Assert.AreEqual(1, record2.Path.Count);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("GBP", "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN"),
+            record2.Path[0]);
+        Assert.AreEqual("60.0000000", record2.SourceAmount);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("USD", "GBRAOXQDNQZRDIOK64HZI4YRDTBFWNUYH3OIHQLY4VEK5AIGMQHCLGXI"),
+            record2.SourceAsset);
 
-        Assert.AreEqual(pathsPage.Records[2].DestinationAmount, "200.0000000");
-        Assert.AreEqual(pathsPage.Records[2].DestinationAsset,
-            Asset.CreateNonNativeAsset("EUR", "GBRCOBK7C7UE72PB5JCPQU3ZI45ZCEM7HKQ3KYV3YD3XB7EBOPBEDN2G"));
-        Assert.AreEqual(pathsPage.Records[2].Path.Count, 3);
-        Assert.AreEqual(pathsPage.Records[2].Path[0],
-            Asset.CreateNonNativeAsset("GBP", "GAX7B3ZT3EOZW5POAMV4NGPPKCYUOYW2QQDIAF23JAXF72NMGRYPYOPM"));
-        Assert.AreEqual(pathsPage.Records[2].Path[1],
-            Asset.CreateNonNativeAsset("PLN", "GACWIA2XGDFWWN3WKPX63JTK4S2J5NDPNOIVYMZY6RVTS7LWF2VHZLV3"));
-        Assert.AreEqual(pathsPage.Records[2].Path[2], new AssetTypeNative());
-        Assert.AreEqual(pathsPage.Records[2].SourceAmount, "300.0000000");
-        Assert.AreEqual(pathsPage.Records[2].SourceAsset,
-            Asset.CreateNonNativeAsset("USD", "GC7J5IHS3GABSX7AZLRINXWLHFTL3WWXLU4QX2UGSDEAIAQW2Q72U3KH"));
+        var record3 = pathsPage.Records[2];
+        Assert.AreEqual("200.0000000", record3.DestinationAmount);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("EUR", "GBRCOBK7C7UE72PB5JCPQU3ZI45ZCEM7HKQ3KYV3YD3XB7EBOPBEDN2G"),
+            record3.DestinationAsset);
+        Assert.AreEqual(3, record3.Path.Count);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("GBP", "GAX7B3ZT3EOZW5POAMV4NGPPKCYUOYW2QQDIAF23JAXF72NMGRYPYOPM"),
+            record3.Path[0]);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("PLN", "GACWIA2XGDFWWN3WKPX63JTK4S2J5NDPNOIVYMZY6RVTS7LWF2VHZLV3"),
+            record3.Path[1]);
+        Assert.AreEqual(new AssetTypeNative(), record3.Path[2]);
+        Assert.AreEqual("300.0000000", record3.SourceAmount);
+        Assert.AreEqual(Asset.CreateNonNativeAsset("USD", "GC7J5IHS3GABSX7AZLRINXWLHFTL3WWXLU4QX2UGSDEAIAQW2Q72U3KH"),
+            record3.SourceAsset);
     }
 }
